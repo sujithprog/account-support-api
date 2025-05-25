9f28b3556fdbff5aae2ece96d8b5b549e7df76f1
sujithprog <138554965+sujithprog@users.noreply.github.com>
2025-05-25 13:55:53 -0500

    Add files via upload

---
+++ data/invoice_db.py
@@ -0,0 +1,12 @@
+INVOICES = {
+    "12345": "Invoice #INV001 - $75.32 - Due 2025-06-01",
+    "23456": "Invoice #INV002 - $0.00 - Paid",
+    "34567": "Invoice #INV003 - $192.10 - Due 2025-06-10",
+    "45678": "Invoice #INV004 - $8.00 - Due 2025-06-03",
+    "56789": "Invoice #INV005 - $130.55 - Due 2025-06-15",
+    "67890": "Invoice #INV006 - $45.00 - Due 2025-06-05",
+    "78901": "Invoice #INV007 - $99.99 - Paid",
+    "89012": "Invoice #INV008 - $21.80 - Due 2025-06-08",
+    "90123": "Invoice #INV009 - $10.10 - Due 2025-06-02",
+    "01234": "Invoice #INV010 - $0.45 - Paid"
+}
