28f6c71b7b2088f537bb47f599fcd7d2149a5987
sujithprog <138554965+sujithprog@users.noreply.github.com>
2025-06-05 20:37:11 -0500

    Add predefined emails and confirmation endpoint

---
+++ data/email_db.py
@@ -0,0 +1,12 @@
+EMAILS = {
+    "12345": "user12345@example.com",
+    "23456": "user23456@example.com",
+    "34567": "user34567@example.com",
+    "45678": "user45678@example.com",
+    "56789": "user56789@example.com",
+    "67890": "user67890@example.com",
+    "78901": "user78901@example.com",
+    "89012": "user89012@example.com",
+    "90123": "user90123@example.com",
+    "01234": "user01234@example.com",
+}
